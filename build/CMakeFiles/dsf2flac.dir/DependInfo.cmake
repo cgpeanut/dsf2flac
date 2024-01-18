0e2e7707f1d851677ea497143064c19f0e3fe145
Roberto Roxas <robertoruizroxas@gmail.com>
2024-01-18 09:52:37 -0600

    compiled

---
+++ build/CMakeFiles/dsf2flac.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/roxasrr/Codes/dsf2flac/src/cmdline.c" "CMakeFiles/dsf2flac.dir/src/cmdline.c.o" "gcc" "CMakeFiles/dsf2flac.dir/src/cmdline.c.o.d"
+  "/home/roxasrr/Codes/dsf2flac/src/libdstdec/ccp_calc.c" "CMakeFiles/dsf2flac.dir/src/libdstdec/ccp_calc.c.o" "gcc" "CMakeFiles/dsf2flac.dir/src/libdstdec/ccp_calc.c.o.d"
+  "/home/roxasrr/Codes/dsf2flac/src/libdstdec/dst_ac.c" "CMakeFiles/dsf2flac.dir/src/libdstdec/dst_ac.c.o" "gcc" "CMakeFiles/dsf2flac.dir/src/libdstdec/dst_ac.c.o.d"
+  "/home/roxasrr/Codes/dsf2flac/src/libdstdec/dst_data.c" "CMakeFiles/dsf2flac.dir/src/libdstdec/dst_data.c.o" "gcc" "CMakeFiles/dsf2flac.dir/src/libdstdec/dst_data.c.o.d"
+  "/home/roxasrr/Codes/dsf2flac/src/libdstdec/dst_fram.c" "CMakeFiles/dsf2flac.dir/src/libdstdec/dst_fram.c.o" "gcc" "CMakeFiles/dsf2flac.dir/src/libdstdec/dst_fram.c.o.d"
+  "/home/roxasrr/Codes/dsf2flac/src/libdstdec/dst_init.c" "CMakeFiles/dsf2flac.dir/src/libdstdec/dst_init.c.o" "gcc" "CMakeFiles/dsf2flac.dir/src/libdstdec/dst_init.c.o.d"
+  "/home/roxasrr/Codes/dsf2flac/src/libdstdec/unpack_dst.c" "CMakeFiles/dsf2flac.dir/src/libdstdec/unpack_dst.c.o" "gcc" "CMakeFiles/dsf2flac.dir/src/libdstdec/unpack_dst.c.o.d"
+  "/home/roxasrr/Codes/dsf2flac/src/dop_packer.cpp" "CMakeFiles/dsf2flac.dir/src/dop_packer.cpp.o" "gcc" "CMakeFiles/dsf2flac.dir/src/dop_packer.cpp.o.d"
+  "/home/roxasrr/Codes/dsf2flac/src/dsd_decimator.cpp" "CMakeFiles/dsf2flac.dir/src/dsd_decimator.cpp.o" "gcc" "CMakeFiles/dsf2flac.dir/src/dsd_decimator.cpp.o.d"
+  "/home/roxasrr/Codes/dsf2flac/src/dsd_sample_reader.cpp" "CMakeFiles/dsf2flac.dir/src/dsd_sample_reader.cpp.o" "gcc" "CMakeFiles/dsf2flac.dir/src/dsd_sample_reader.cpp.o.d"
+  "/home/roxasrr/Codes/dsf2flac/src/dsdiff_file_reader.cpp" "CMakeFiles/dsf2flac.dir/src/dsdiff_file_reader.cpp.o" "gcc" "CMakeFiles/dsf2flac.dir/src/dsdiff_file_reader.cpp.o.d"
+  "/home/roxasrr/Codes/dsf2flac/src/dsf_file_reader.cpp" "CMakeFiles/dsf2flac.dir/src/dsf_file_reader.cpp.o" "gcc" "CMakeFiles/dsf2flac.dir/src/dsf_file_reader.cpp.o.d"
+  "/home/roxasrr/Codes/dsf2flac/src/filters.cpp" "CMakeFiles/dsf2flac.dir/src/filters.cpp.o" "gcc" "CMakeFiles/dsf2flac.dir/src/filters.cpp.o.d"
+  "/home/roxasrr/Codes/dsf2flac/src/fstream_plus.cpp" "CMakeFiles/dsf2flac.dir/src/fstream_plus.cpp.o" "gcc" "CMakeFiles/dsf2flac.dir/src/fstream_plus.cpp.o.d"
+  "/home/roxasrr/Codes/dsf2flac/src/main.cpp" "CMakeFiles/dsf2flac.dir/src/main.cpp.o" "gcc" "CMakeFiles/dsf2flac.dir/src/main.cpp.o.d"
+  "/home/roxasrr/Codes/dsf2flac/src/tagConversion.cpp" "CMakeFiles/dsf2flac.dir/src/tagConversion.cpp.o" "gcc" "CMakeFiles/dsf2flac.dir/src/tagConversion.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/dsf2flac.dir/cmake_clean.cmake
@@ -0,0 +1,41 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dsf2flac.dir/src/cmdline.c.o"
+  "CMakeFiles/dsf2flac.dir/src/cmdline.c.o.d"
+  "CMakeFiles/dsf2flac.dir/src/dop_packer.cpp.o"
+  "CMakeFiles/dsf2flac.dir/src/dop_packer.cpp.o.d"
+  "CMakeFiles/dsf2flac.dir/src/dsd_decimator.cpp.o"
+  "CMakeFiles/dsf2flac.dir/src/dsd_decimator.cpp.o.d"
+  "CMakeFiles/dsf2flac.dir/src/dsd_sample_reader.cpp.o"
+  "CMakeFiles/dsf2flac.dir/src/dsd_sample_reader.cpp.o.d"
+  "CMakeFiles/dsf2flac.dir/src/dsdiff_file_reader.cpp.o"
+  "CMakeFiles/dsf2flac.dir/src/dsdiff_file_reader.cpp.o.d"
+  "CMakeFiles/dsf2flac.dir/src/dsf_file_reader.cpp.o"
+  "CMakeFiles/dsf2flac.dir/src/dsf_file_reader.cpp.o.d"
+  "CMakeFiles/dsf2flac.dir/src/filters.cpp.o"
+  "CMakeFiles/dsf2flac.dir/src/filters.cpp.o.d"
+  "CMakeFiles/dsf2flac.dir/src/fstream_plus.cpp.o"
+  "CMakeFiles/dsf2flac.dir/src/fstream_plus.cpp.o.d"
+  "CMakeFiles/dsf2flac.dir/src/libdstdec/ccp_calc.c.o"
+  "CMakeFiles/dsf2flac.dir/src/libdstdec/ccp_calc.c.o.d"
+  "CMakeFiles/dsf2flac.dir/src/libdstdec/dst_ac.c.o"
+  "CMakeFiles/dsf2flac.dir/src/libdstdec/dst_ac.c.o.d"
+  "CMakeFiles/dsf2flac.dir/src/libdstdec/dst_data.c.o"
+  "CMakeFiles/dsf2flac.dir/src/libdstdec/dst_data.c.o.d"
+  "CMakeFiles/dsf2flac.dir/src/libdstdec/dst_fram.c.o"
+  "CMakeFiles/dsf2flac.dir/src/libdstdec/dst_fram.c.o.d"
+  "CMakeFiles/dsf2flac.dir/src/libdstdec/dst_init.c.o"
+  "CMakeFiles/dsf2flac.dir/src/libdstdec/dst_init.c.o.d"
+  "CMakeFiles/dsf2flac.dir/src/libdstdec/unpack_dst.c.o"
+  "CMakeFiles/dsf2flac.dir/src/libdstdec/unpack_dst.c.o.d"
+  "CMakeFiles/dsf2flac.dir/src/main.cpp.o"
+  "CMakeFiles/dsf2flac.dir/src/main.cpp.o.d"
+  "CMakeFiles/dsf2flac.dir/src/tagConversion.cpp.o"
+  "CMakeFiles/dsf2flac.dir/src/tagConversion.cpp.o.d"
+  "dsf2flac"
+  "dsf2flac.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/dsf2flac.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
